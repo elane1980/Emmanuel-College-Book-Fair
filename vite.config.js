1f87c147ede4c084aa88955d8b6829cf0bdd0c4c
elane1980 <eliselanes1@gmail.com>
2025-03-28 10:20:13 +0000

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,12 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: 'index.html'
+    }
+  },
+  server: {
+    historyApiFallback: true
+  }
 });
